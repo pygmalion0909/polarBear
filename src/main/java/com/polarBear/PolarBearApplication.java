cc932d514dd10b30c8839a069178bbf42d1c9ca8
superpil <superpil@trgtech.co.kr>
2020-11-09 08:34:49 +0900

    init최초등록

---
+++ src/main/java/com/polarBear/PolarBearApplication.java
@@ -0,0 +1,13 @@
+package com.polarBear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolarBearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolarBearApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/polarBear/PolarBearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polarBear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolarBearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
